044531618e589d716da9d1956119714663e9c43f
jmuria <jmuria@gmail.com>
2013-01-15 21:57:53 +0100

    Solution reader ya crea projects vacios

---
+++ HellBlaster/Domain/VS10SolutionReader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HellBlaster.Domain
+{
+	public class VS10SolutionReader
+	{
+
+		private static string ProjectTag	{get{return "Project(\"{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}\")";}}
+
+		public List<VS10Project> FindReferences(string solutionfilecontent)
+		{
+			if (!String.IsNullOrEmpty(solutionfilecontent) && solutionfilecontent.Contains("Microsoft Visual Studio Solution File, Format Version 11.00"))
+				return FindProjectsInSolutionFile(solutionfilecontent);
+			else
+				return null;
+		}
+
+		private static List<VS10Project> FindProjectsInSolutionFile(string solutionfilecontent)
+		{
+			List<VS10Project> projects = new List<VS10Project>();
+			string[] lines = solutionfilecontent.Split('\n');
+			IEnumerable<string> linesWithProjects = from line in lines
+													where line.IndexOf(ProjectTag) == 0
+													select line;
+			foreach (string project in linesWithProjects)
+			{
+				projects.Add(new VS10Project());
+			}
+			return projects;
+		}
+
+
+	}
+}
---
+++ HellBlasterTest/Tests/SolutionReaderTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using HellBlaster.Domain;
+
+namespace HellBlasterTest.Tests
+{
+	[TestFixture]
+	public class SolutionReaderTests
+	{
+		private string vs10withOneProject=
+"Microsoft Visual Studio Solution File, Format Version 11.00\r\n"+
+"# Visual Studio 2010\r\n"+
+"Project(\"{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}\") = \"HellBlaster\", \"HellBlaster\\HellBlaster.csproj\", \"{D2AB4C1B-7F1A-452A-BE7B-A432C25C8316}\"\r\n"+
+"EndProject\r\n"+
+"Project(\"{2150E333-8FDC-42A3-9474-1A3956D46DE8}\") = \"Solution Items\", \"Solution Items\", \"{9DAEDB08-A3A9-411A-88C6-00337FAEE0EE}\"\r\n"+
+	"\tProjectSection(SolutionItems) = preProject\r\n"+
+		"\t\tHellBlaster.vsmdi = HellBlaster.vsmdi\r\n"+
+		"\t\tLocal.testsettings = Local.testsettings\r\n"+
+		"\t\tTraceAndTestImpact.testsettings = TraceAndTestImpact.testsettings\r\n"+
+	"\tEndProjectSection\r\n"+
+"EndProject\r\n"+
+"Project(\"{2150E333-8FDC-42A3-9474-1A3956D46DE8}\") = \"libs\", \"libs\", \"{F6B6A0B3-6182-4C34-83EC-AD41CD215CF1}\"\r\n"+
+"EndProject\r\n"+
+"Global\r\n"+
+	"\tGlobalSection(TestCaseManagementSettings) = postSolution\r\n"+
+		"\t\tCategoryFile = HellBlaster.vsmdi\r\n"+
+	"\tEndGlobalSection	\r\n"+
+"EndGlobal\r\n";
+
+
+		[Test]
+		public void WhenTheStringIsEmptyIGetANUllReferenceList()
+		{
+			VS10SolutionReader pr = new VS10SolutionReader();
+			Assert.IsNull(pr.FindReferences(null as string));
+		}
+
+
+		[Test]
+		public void WhenTheStringIsNotAnXMLIGetANullReferenceList()
+		{
+			VS10SolutionReader pr = new VS10SolutionReader();
+			Assert.IsNull(pr.FindReferences("This is not a xml"));
+		}
+
+		[Test]
+		public void WhenTheStringIsNotAnVS10FileIGetANullReferenceList()
+		{
+			VS10SolutionReader pr = new VS10SolutionReader();
+			Assert.IsNull(pr.FindReferences("Microsoft Visual Studio Solution File, Format Version 9.00"));
+		}
+
+		[Test]
+		public void WhenTheStringContainsAProjectIGetReferenceListWithOneItem()
+		{
+			VS10SolutionReader pr = new VS10SolutionReader();
+
+			List<VS10Project> refs = pr.FindReferences(vs10withOneProject);
+			Assert.IsNotNull(refs);
+			Assert.AreEqual(1, refs.Count);
+		}
+	}
+}
