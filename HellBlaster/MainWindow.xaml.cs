f8d7d74c09db83d0088e62c4370797503dbbb584
jmuria <jmuria@gmail.com>
2013-01-31 22:17:21 +0100

    Simple View and prepared test for a file reference without version

--- HellBlaster/MainWindow.xaml.cs
+++ HellBlaster/MainWindow.xaml.cs
@@ -11,17 +11,70 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HellBlaster.Interfaces;
+using HellBlaster.Controllers;
 
 namespace HellBlaster
 {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class MainWindow : Window,IMainPageView
 	{
+		MainPageCtrl ctrl;
 		public MainWindow()
 		{
+			ctrl = new MainPageCtrl();
+			ctrl.View = this;
 			InitializeComponent();
+		}
+
+
+
+		public void AddProject(string projectName)
+		{
+			TextBlock tb = new TextBlock();
+			tb.Text = projectName;
+			Projects.Children.Add(tb);
+		}
+
+		public void AddFileRefence(string name, string version)
+		{
+			TextBlock tb = new TextBlock();
+			tb.Text = name+":"+version;
+			Projects.Children.Add(tb);
+		}
+
+		public void UpdateFileRefence(string projectName, string assemblyName, string assemblyVersion)
+		{
+			foreach (TextBlock tb in Projects.Children)
+			{
+				if (tb.Text.Contains(assemblyName))
+				{
+					tb.Text = assemblyName + ":" + assemblyVersion;
+				}
+			}
+		}
+
+		private void Select_Click(object sender, RoutedEventArgs e)
+		{
+			Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+			dlg.DefaultExt = ".sln";
+			dlg.Filter = "Visual Studio Solution files (.sln)|*.sln";
+
+			Nullable<bool> result = dlg.ShowDialog();
+
+			if (result == true)
+			{
+				string filename = dlg.FileName;
+				SolutionPath.Text = filename;
+			}
+		}
+
+		private void Analyze_Click(object sender, RoutedEventArgs e)
+		{
+			ctrl.LoadSolutionFile(SolutionPath.Text);
 		}
 	}
 }
--- HellBlasterTest/Tests/ProjectReaderTests.cs
+++ HellBlasterTest/Tests/ProjectReaderTests.cs
@@ -12,6 +12,7 @@ namespace HellBlasterTest.Tests
 	{
 
 		string vs10withOneFileReference;
+		string vs10withTwoFileReferences;
 
 		[SetUp]
 		public void SetUp()
@@ -31,6 +32,38 @@ namespace HellBlasterTest.Tests
 				   + "</Reference>    "
 				 + "</ItemGroup>"
 			   + "</Project>";
+
+
+			 vs10withTwoFileReferences = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n" +
+ "<Project ToolsVersion=\"4.0\" DefaultTargets=\"Build\" xmlns=\"http://schemas.microsoft.com/developer/msbuild/2003\">\r\n" +
+   "<PropertyGroup>\r\n" +
+	 "<Configuration Condition=\" '$(Configuration)' == '' \">Debug</Configuration>\r\n" +
+	 "<Platform Condition=\" '$(Platform)' == '' \">AnyCPU</Platform>    \r\n" +
+   "</PropertyGroup>\r\n" +
+  "\r\n" +
+ "  <ItemGroup>\r\n" +
+	 "<Reference Include=\"Microsoft.VisualStudio.QualityTools.UnitTestFramework, Version=10.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a, processorArchitecture=MSIL\" />\r\n" +
+	 "<Reference Include=\"NSubstitute\">\r\n" +
+	   "<HintPath>..\\packages\\NSubstitute.1.4.2.0\\lib\\NET40\\NSubstitute.dll</HintPath>\r\n" +
+	 "</Reference>\r\n" +
+	 "<Reference Include=\"nunit.framework, Version=2.6.1.12217, Culture=neutral, PublicKeyToken=96d09a1eb7f44a77, processorArchitecture=MSIL\">\r\n" +
+	   "<SpecificVersion>False</SpecificVersion>\r\n" +
+	   "<HintPath>Libraries\\nunit.framework.dll</HintPath>\r\n" +
+	 "</Reference>\r\n" +
+	 "<Reference Include=\"System\" />\r\n" +
+	 "<Reference Include=\"System.Core\">\r\n" +
+	   "<RequiredTargetFramework>3.5</RequiredTargetFramework>\r\n" +
+	 "</Reference>\r\n" +
+   "</ItemGroup>\r\n" +
+   "<ItemGroup>\r\n" +
+	 "<CodeAnalysisDependentAssemblyPaths Condition=\" '$(VS100COMNTOOLS)' != '' \" Include=\"$(VS100COMNTOOLS)..\\IDE\\PrivateAssemblies\">\r\n" +
+	   "<Visible>False</Visible>\r\n" +
+	 "</CodeAnalysisDependentAssemblyPaths>\r\n" +
+   "</ItemGroup>\r\n" +
+ "</Project>\r\n";
+
+
+
 		}
 
 		[Test]
@@ -93,5 +126,17 @@ namespace HellBlasterTest.Tests
 			List<FileReference> refs = pr.FindFileReferences(vs10withOneFileReference);
 			Assert.AreEqual("nunit.framework", refs[0].Name);
 		}
+
+		[Test]
+		public void WhenTheProjectHasTwoReferencesICanRetrieveThem()
+		{
+			VS10ProjectReader pr = new VS10ProjectReader();
+
+			List<FileReference> refs = pr.FindFileReferences(vs10withTwoFileReferences);
+			Assert.AreEqual("NSubstitute", refs[0].Name);
+			Assert.AreEqual("nunit.framework", refs[1].Name);
+		}
+
+		
 	}
 }
