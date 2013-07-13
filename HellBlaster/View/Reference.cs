f489edf1aeebbed3c97a1cfda98bd9119a4858b8
jmuria <jmuria@gmail.com>
2013-07-13 16:14:41 +0200

    The UI wan't updated when the version changed

--- HellBlaster/View/Reference.cs
+++ HellBlaster/View/Reference.cs
@@ -2,14 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ComponentModel;
 
 namespace HellBlaster.View
 {
-	public class Reference
+	public class Reference : INotifyPropertyChanged
 	{
 		public string projectName { get; set; }
 		public string Name { get; set; }
-		public string version { get; set; }
+
+		private string myversion;
+		public string version { get { return myversion; } set { myversion = value; OnPropertyChanged("version"); } }
+
+		private void OnPropertyChanged(string name)
+		{
+			PropertyChangedEventHandler handler = PropertyChanged;
+			if (handler != null)
+			{
+				handler(this, new PropertyChangedEventArgs(name));
+			}
+		}
+
 		public bool hasDiscrepancy { get; set; }
+
+		public event PropertyChangedEventHandler PropertyChanged;
 	}
 }
