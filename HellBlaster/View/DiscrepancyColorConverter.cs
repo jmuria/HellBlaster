024b43f7dbcc63b6df62b9131ea968f4b382a100
jmuria <jmuria@gmail.com>
2013-11-30 17:32:51 +0100

    Discrepancy column with color

---
+++ HellBlaster/View/DiscrepancyColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace HellBlaster
+{
+	public class DiscrepancyColorConverter : IValueConverter
+	{
+
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			if ((value as bool?).GetValueOrDefault() == true)
+				return new SolidColorBrush(Colors.Red);
+			else
+				return new SolidColorBrush(Colors.Green);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
